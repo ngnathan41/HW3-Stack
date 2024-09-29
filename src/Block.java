56c613c9a58473dc8b8933ea1e2a9a02f5fb3624
eoggod <ngn@bxscience.edu>
2024-09-29 17:29:19 -0400

    mostly finished

--- src/Block.java
+++ src/Block.java
@@ -9,16 +9,26 @@ public class Block {
 
     public void addValue(INTitialValue value){
         values[numValues] = value;
+        numValues++;
     }
 
-    public String returnValues(){
+    public String toString(){
         StringBuilder table = new StringBuilder();
-        String format = "%-13s %-13s";
+        String format = "%-13s %-13s \n";
+        table.append(String.format(format, "Variable Name", "Initial Value"));
         for (int i = 0; i < numValues; i++) {
             table.append(String.format(format, values[i].getName(), values[i].getValue()));
         }
 
         return table.toString();
+    }
+
+    public int[] findValue(String name){
+        for (int i = 0; i < numValues; i++) {
+            if(values[i].getName().equals(name))
+                return new int[]{1, values[i].getValue()};
+        }
+        return new int[0];
     }
 
 
--- src/BlockTracer.java
+++ src/BlockTracer.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -20,14 +17,14 @@ public class BlockTracer {
                 parse(stdin);
                 break;
             }
-            catch(FileNotFoundException e){
+            catch(Exception e){
                 System.out.println("File not found.");
             }
         }
         sc.close();
     }
 
-    private static void parse(BufferedReader stdin){
+    private static void parse(BufferedReader stdin) throws IOException {
         Stack<Block> blockStack = new Stack<>();
         String line;
 
@@ -38,8 +35,48 @@ public class BlockTracer {
                 blockStack.push(new Block());
             else if (line.contains("}"))
                 blockStack.pop();
-            else if (line.)
+            else if (line.startsWith("int ")) {
+                addVars(line, blockStack.peek());
+            }
+            else if (line.contains("/*$print LOCAL*/"))
+                System.out.println(blockStack.peek());
+            else if (line.contains("/*$print ")) {
+                printVariable(line, blockStack);
+            }
+
         }
     }
+
+    private static void addVars(String line, Block block){
+        line = line.replace("int ", "").replace(";", "");
+        String[] vars = line.split(",");
+        for (String var: vars){
+            String[] parts = var.split("=");
+            int value = (parts.length >1) ? Integer.parseInt(parts[1].trim()) : 0;
+            block.addValue(new INTitialValue(parts[0].trim(), value));
+        }
+    }
+
+    private static void printVariable(String line, Stack<Block> blockStack){
+        String name = line.split(" ")[1].split("\\*")[0];
+        Stack<Block> temp = new Stack<>();
+        while(!blockStack.isEmpty()){
+            temp.push(blockStack.pop());
+            int[] res = temp.peek().findValue(name);
+            if(res[0] == 1) {
+                StringBuilder table = new StringBuilder();
+                String format = "%-13s %-13s \n";
+                table.append(String.format(format, "Variable Name", "Initial Value"));
+                table.append(String.format(format, name, res[1]));
+                System.out.println(table);
+                break;
+            }
+        }
+
+        while(!temp.isEmpty()){
+            blockStack.push(temp.pop());
+        }
+    }
+
 
 }
